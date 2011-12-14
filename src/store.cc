a33e4089e463d175014d57afed74d3897a7de02d
Julien Ammous <schmurfy@gmail.com>
2011-12-14 15:39:07 +0100

    commented debug line

--- src/store.cc
+++ src/store.cc
@@ -372,9 +372,9 @@ StoreEntry::deferProducer(const AsyncCall::Pointer &producer)
 {
     if (!deferredProducer)
         deferredProducer = producer;
-    else
-        debugs(20, 5, HERE << "Deferred producer call is allready set to: " <<
-               *deferredProducer << ", requested call: " << *producer);
+    // else
+    //     debugs(20, 5, HERE << "Deferred producer call is allready set to: " <<
+    //            *deferredProducer << ", requested call: " << *producer);
 }
 
 void
